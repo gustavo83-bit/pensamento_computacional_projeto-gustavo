33624a706abe855002910bb8aeaec862b9025e75
GUSTAVO RODRIGUES ARAUJO <rodrigues25gustavo@gmail.com>
2026-03-11 18:03:56 -0300

    Add files via upload

---
+++ gustavo/ead.py
@@ -0,0 +1,44 @@
+print('\m Calculadoora simples - Python Vocacao\n')
+
+numb_hum = input("Digite o primeiro numero:")
+numb_dois = input('Digite o segundo numero:')
+
+
+operar_numb = input('escolha a operação: 1 -> +, 2 -> -, 3 -> *, 4 ->/')
+
+                       
+if operar_numb == '4':
+    result = int(numb_hum) / int(numb_dois)
+    print(f'o resultado é {result}')
+
+elif operar_numb == '3':
+    result = int(numb_hum) / int(numb_dois)
+    print(f'o resultado é {result}')
+
+
+elif operacao == '4':
+    if int(num_dois) != 0:
+        result = int(num_um) / int(num_dois)
+        print(f'o resultado da divisao é: {result}')
+    else:
+        print("erro: divisao por zero nao é permitida")
+
+else:
+    ("numero nao e valido, tente novamente!")
+
+
+    frutas = ['maçã', 'banana', 'laranja',abacaxi]
+
+    frutas.append('amora')
+    frutas.append('mertilo')
+    frutas.append('melancia')
+
+
+    print(frutas[5])
+
+
+
+
+
+
+
