d42c57d2f4baf92bf893fa629db779bad99b7661
GUSTAVO RODRIGUES ARAUJO <rodrigues25gustavo@gmail.com>
2026-03-25 18:05:22 -0300

    Add files via upload

---
+++ aula online atividade.py
@@ -0,0 +1,12 @@
+#1. Definimos a função com o nome 'saudar'
+# O que está entre parênteses (nome) é chamado de PARÂMETRO
+def saudar(nome):
+    """
+    Esta função recebe um nome e exibe uma saudação.
+    """
+    print(f"Olá, {nome}! Bem-vindo ao mundo da programação.")
+
+# 2. Chamamos (executamos) a função
+# Passamos o valor "Tiago" para dentro do parâmetro 'nome'
+saudar("Tiago")
+saudar("Maria")
